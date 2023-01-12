dde983fea5f1012a491b986d8077ed086a62ef52
Kelio <keliojosue@outlook.com>
2023-01-12 14:16:24 -0300

    Less chance to have an incomplete translation

--- translate.js
+++ translate.js
@@ -1,12 +1,22 @@
 import fetch from "node-fetch";
 
-export async function translate(text, sourceLang, destLang) {
-  const endpoint = "https://translate.googleapis.com/translate_a/single";
+async function _translate(text, sourceLang, destLang) {
+  const endpoint = "http://translate.google.com/translate_a/single";
   const response = await fetch(
     `${endpoint}?client=gtx&sl=${sourceLang}&tl=${destLang}&dt=t&q=${encodeURIComponent(text)}`
   );
   const data = await response.json();
   return data[0][0][0];
+}
+
+export async function translate(text, sourceLang, destLang) {
+  const texts = text
+    .split(".")
+    .map((t) => t.trim())
+    .filter((t) => t !== "");
+
+  const translations = await Promise.all(texts.map((t) => _translate(t, sourceLang, destLang)));
+  return translations.map((t) => t.trim()).join(". ");
 }
 
 export async function retranslate(text, sourceLang, destLang) {
